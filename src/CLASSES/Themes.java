05aee078dcd0b3240fb38ddba015392dbaa1359f
Suharsha <suharshapw@hmail.com>
2014-08-22 02:35:57 +0530

    Initial commit

---
+++ src/CLASSES/Themes.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CLASSES;
+
+import de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel;
+import de.javasoft.plaf.synthetica.SyntheticaBlueMoonLookAndFeel;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Suharsha Wick
+ */
+public class Themes {
+    public void setTheme(int id) {
+        try 
+        {
+            if (id==1)
+                UIManager.setLookAndFeel(new SyntheticaAluOxideLookAndFeel());
+            else if (id==2)
+                UIManager.setLookAndFeel(new SyntheticaBlueMoonLookAndFeel());
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public void setIcon(JFrame j) {
+        j.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/IMAGES/favicon.png")));
+    }
+    
+}
